7e63652a9cea40280dbcc62c2dedcfa0b631f6f5
Stefan Ziegler <edi.gonzales@gmail.com>
2016-03-30 21:52:25 +0200

    insert into model.sql

---
+++ db/insert_into_model.sql
@@ -0,0 +1,2 @@
+INSERT INTO ai_rest_api.model (canton, aname, schema_language, version, repository) VALUES
+    ('SO', 'SO_AV_Nachfuehrungskreise_20160304', 'ili2.3', '2016-03-04', 'http://www.catais.org/models');
